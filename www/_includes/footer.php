a853851827edc7669ec45d6b2d14169f14ff7d71
rsmb <rsmbryson@gmail.com>
2014-11-03 10:51:28 -0700

    Footer update to Laurie's Email
    Changed the footer to be readable and to the correct email address.

--- www/_includes/footer.php
+++ www/_includes/footer.php
@@ -1,12 +1,1 @@
-<div align="center">
-        	<strong>Verde River Greenway</strong><br />
-            PO Box 2535, Cottonwood, AZ<br />
-            <script language="JavaScript">safemail('','info','Email Us Today')</script>
-      </div>
-      
-  <div id="copyright">
-      		Copyright &copy; <?=date("Y")?>. Verde River Greenway &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            Designed by: <a href="http://www.web-writer.com" target="_blank">Web-Writer</a>
-      </div>
-       <div class="clear-floats"></div>
-    </div>
+<div align="center">    <strong>Verde River Greenway</strong>    <br />    PO Box 2535, Cottonwood, AZ    <br />    <script language="JavaScript">safemail('','admin','Email Us Today')</script></div><div id="copyright">    Copyright &copy; <?=date("Y")?>. Verde River Greenway &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;            Designed by: <a href="http://www.web-writer.com" target="_blank">Web-Writer</a></div><div class="clear-floats"></div>
