d425590891bb95a9c3bb6794a7e9154167bf0cb8
rsmb <rsmbryson@gmail.com>
2014-11-13 09:13:47 -0700

    Cleaned up old site pages
    Some unused pages still exist/are hit on occasion. become-member.php and
    donate-original.php have been changed to redirects to donate.php.

--- www/donate-original.php
+++ www/donate-original.php
@@ -1,88 +1,1 @@
-<?php
-	require('_includes/setup.php');
-	$pagetitle = 'Donate';
-	$pagekeywords = '';
-	$pagedesc = '';
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link href="css/pageinfo.css" rel="stylesheet" type="text/css" />
-
-<?php include ("_includes/metatags.php"); ?>
-</head>
-
-
-<body>
-
-<?php
-$a_browser_data = browser_detection('full');
-if ( $a_browser_data[0] == 'ie' && $a_browser_data[1] <= 6)
-{
-	echo "The site is designed for Microsoft Internet Explorer 7+. To download IE 7, please click <a href=\"http://www.microsoft.com/downloads/details.aspx?familyid=9AE91EBE-3385-447C-8A30-081805B2F90B&amp;displaylang=en\" target=\"_blank\">here</a>.<br><br>";
-}
-?>
-
-<div id="page-top"><img src="images/bird-top.jpg" alt="Arizona&rsquo;s last free-flowing rivers" width="359" height="63" border="0" align="top" /></div>
-
-<div id="page-container">
-	
-	<div id="lefttop">
-    	<img src="images/leftuppercorner.jpg" alt="Friends of Verde River " width="19" height="62" border="0" align="bottom" />
-    </div>
-    
-	<div id="content-container">
-   	  <img src="images/header.jpg" alt="Verde River Greenway" width="974" height="165" border="0" align="top" usemap="#home" />
-   		<map name="home" id="home">
-   		  <area shape="rect" coords="502,12,950,98" href="index.php" />
-       	</map>
-        
-<div id="interface">
-   	 	 <?php include ("_includes/interfacebar.php"); ?>
-    </div>
-    <div id="header">
-   	  <h1>Donate to the Friends of Verde River Greenway</h1>
-   		<img src="images/header-lines.jpg" width="900" height="8" border="0" align="top" /> </div>
-    <div id="content-left">
-    If you are interested in donating to the Friends of Verde River Greenway, please click the Donate button below.<br />
-    <br />
-      As an affiliate of Arizona State Parks Foundation, Friends of Verde River Greenway is a tax-exempt organization and the Taxpayer  Identification Number is 20-1196825.   Your support is vital to our mission of preserving and enhancing the beautiful  Verde River Greenway.
-<br /><br /><br />
-      <div align="center"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="BGKBL3CL6ZDQ4">
-<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - Donate Now">
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-</div>
-      <br />
-    </div>  <!-- Close Content Left Box-->
-    
-    <div id="content-right">
-    
-    	<?php include ("_includes/eventbox1.php"); ?>
-        
-    
-    </div>  <!-- Close Content Right Box--> 
-    
-    
-  <div class="clear-floats"></div>
-  
-    <div id="interface-bottom-holder">
-   	  <?php include ("_includes/interfacebar-bottom.php"); ?>
-      
-    </div>
-    
-    <div id="footer">
-    <br />
-    	
-		<?php include ("_includes/footer.php"); ?>
-	</div> <!-- Close Footer-->
-  
-  </div> <!-- Close Content Holder Box-->
-   
-</div> <!-- Close Page Content Box-->
-</body>
-</html>
+<!DOCTYPE HTML><html lang="en-US">    <head>        <meta charset="UTF-8">        <meta http-equiv="refresh" content="1;url=http://www.verderivergreenway.org/donate.php">        <script type="text/javascript">            window.location.href = "http://www.verderivergreenway.org/donate.php"        </script>        <title>Page Redirection</title>    </head>    <body>        <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->        If you are not redirected automatically, follow the <a href='http://www.verderivergreenway.org/donate.php'>link to example</a>    </body></html>
