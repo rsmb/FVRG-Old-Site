8e117cc4b15098d218242d774074bedd40d83af6
rsmb <rsmbryson@gmail.com>
2014-12-02 17:16:45 -0700

    Added donate button in eventbox1

--- www/_includes/eventbox1.php
+++ www/_includes/eventbox1.php
@@ -2,18 +2,8 @@
   <img src="images/event-top.jpg" width="244" height="31" />
 
   <div id="event-txt">
-    <h3>Volunteer Opportunity</h3>
-    <p>
-      Come volunteer with Friends and Yavapai Big Brothers Big Sisters
-      <br />
-      Nov 15, 2014
-      <br />
-      Cottonwood, AZ
-      <br />
-      <a href="PDFs/BB_BS_Friends_20141115.pdf" target="_blank">More information</a>
-      <br />
-      <br />
-      <img src="images/tapco.jpg" width="200" height="137" alt="Tapco River Access Opens" style="float:left; margin-right:6pt; margin-top:4pt; margin-bottom:20pt"/>
-    </p>
+    <a href="https://co.clickandpledge.com/?wid=91656" target="-blank">
+      <img alt="Donate Now" border="0" src="images/Donate_vertical.jpg" />
+    </a>
   </div>
 </div> <!-- Close Event Box 1 Box-->
---
+++ www/_includes/eventbox_YBBBS.php
@@ -0,0 +1,19 @@
+<div id="event-box">
+  <img src="images/event-top.jpg" width="244" height="31" />
+
+  <div id="event-txt">
+    <h3>Volunteer Opportunity</h3>
+    <p>
+      Come volunteer with Friends and Yavapai Big Brothers Big Sisters
+      <br />
+      Nov 15, 2014
+      <br />
+      Cottonwood, AZ
+      <br />
+      <a href="PDFs/BB_BS_Friends_20141115.pdf" target="_blank">More information</a>
+      <br />
+      <br />
+      <img src="images/tapco.jpg" width="200" height="137" alt="Tapco River Access Opens" style="float:left; margin-right:6pt; margin-top:4pt; margin-bottom:20pt"/>
+    </p>
+  </div>
+</div> <!-- Close Event Box 1 Box-->
